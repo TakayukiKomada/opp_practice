38d1f7df2f583be68dbc0cda22836e2d1e033404
TakayukiKomada <Takakoma.79@gmail.com>
2022-06-19 17:48:33 +0900

    if not でバリューエラーを作りました

--- bmi.py
+++ bmi.py
@@ -1,14 +1,19 @@
 class BMI:
     def __init__(self, height, weight):
-        self.height = height
-        self.weight = weight
+        self.value = weight / (height**2)
 
-    def calculate_bmi(self):
-        return self.weight / (self.height**2)
+        if not (10 <= self.value <= 40):
+            raise ValueError
+
+    def __str__(self):
+        return f"{self.value:.2f}"
 
 
-tanaka_bmi = BMI(height=1.80, weight=67.0)
+tanaka_bmi = BMI(height=1.8, weight=67.0)
 sasami_bmi = BMI(height=1.58, weight=80.0)
-print(tanaka_bmi.height, sasami_bmi.height)
 
-print(tanaka_bmi.calculate_bmi())
+print(tanaka_bmi)
+print(sasami_bmi)
+""" BMIはBMIクラスになる。heightとweightはインスタンス変数 calaculate()はインスタンス・メソッドこれらは抽象
+一番したのところで
+ """
