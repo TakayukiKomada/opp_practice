cff6038a53315cf9e188db9a58066d6c6fd76bd4
TakayukiKomada <Takakoma.79@gmail.com>
2022-06-19 16:10:26 +0900

    classの説明をしました

---
+++ username.py
@@ -0,0 +1,26 @@
+""" データ管理: UserName
+    属性:
+        実際のユーザー名
+    ルール:
+        ユーザー名は4文字以上20字以内である
+    できること:
+        ユーザー名を大文字に変換する
+ """
+
+
+class UserName:
+    def __init__(self, name):
+        if not 4 <= len(name) and len(name) <= 20:
+            raise ValueError(f"{name}はルール違反です")
+        self.name = name
+
+    def screen_name(self):
+        return self.name.upper()
+
+
+tanaka = UserName(name="tanaka")
+#bob = UserName(name="bob")
+
+print(tanaka.screen_name())
+#print(bob.screen_name())
+
